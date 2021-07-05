56bc6c86fa6a7df0b41ed25c3977355c1ddc9294
TzachiSh <tzachi.shirazi@gmail.com>
2021-07-05 10:53:47 +0000

    Fix

--- vars/googleChatAddSections.groovy
+++ vars/googleChatAddSections.groovy
@@ -3,27 +3,22 @@
 import static groovy.json.JsonOutput.toJson
 
 void call(final Map<String, String> buildProperties = [:], final String url = env.GOOGLE_BOT_URL) {
-    
+
     final Map<String, Object> complexMessage = [
         buildTag: "${env.BUILD_TAG}",
-        sections: []
+        sections: [ header: "Stage ${STAGE_NAME}", widgets: [] ] 
     ]
 
     if (buildProperties.message) {
         final String message = buildProperties.remove("message")
-        complexMessage.sections << [
-            "widgets": [
-                [
-                    textParagraph: [text: message]
-                ]
-            ]
+        complexMessage.sections.widgets << [
+            textParagraph: [text: message]
         ]
     }
 
     if (buildProperties) {
-        complexMessage.sections << [
-            header: "Stage ${STAGE_NAME}",
-            widgets: buildProperties.collect { key, value ->
+        complexMessage.sections.widgets << [
+            buildProperties.collect { key, value ->
                 [keyValue: [topLabel: "${key}", content: "${value}", contentMultiline: "true"]]
             }
         ]
